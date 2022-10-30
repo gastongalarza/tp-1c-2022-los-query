06ef2c622cac77731406e95bf81d349c78e1f6c2
Ignacio-Ferrante <ignacioferrante1379@gmail.com>
2022-10-30 19:48:30 -0300

    mergeando tablas y corrigiendo el dropeo para que coincida

--- TP 2C/data/temporal-drop.sql
+++ TP 2C/data/temporal-drop.sql
@@ -9,41 +9,17 @@ DROP TABLE INFORMADOS.producto_por_compra
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'compra')
 DROP TABLE INFORMADOS.compra
 
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'producto_por_venta')
-DROP TABLE INFORMADOS.producto_por_venta
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'descuento')
-DROP TABLE INFORMADOS.descuento
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'cupon_por_venta')
-DROP TABLE INFORMADOS.cupon_por_venta
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'venta')
-DROP TABLE INFORMADOS.venta
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'envio')
-DROP TABLE INFORMADOS.envio
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'cliente')
-DROP TABLE INFORMADOS.cliente
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'canal')
-DROP TABLE INFORMADOS.canal
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'metodo_envio')
-DROP TABLE INFORMADOS.metodo_envio
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'medio_pago_venta')
-DROP TABLE INFORMADOS.medio_pago_venta
-
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'medio_pago_compra')
-DROP TABLE INFORMADOS.medio_pago_compra
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'proveedor')
+DROP TABLE INFORMADOS.proveedor
 
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'descuento_compra')
 DROP TABLE INFORMADOS.descuento_compra
 
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'cupon')
-DROP TABLE INFORMADOS.cupon
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'medio_pago_compra')
+DROP TABLE INFORMADOS.medio_pago_compra
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'producto_por_venta')
+DROP TABLE INFORMADOS.producto_por_venta
 
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'variante_producto')
 DROP TABLE INFORMADOS.variante_producto
@@ -60,17 +36,38 @@ DROP TABLE INFORMADOS.producto
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'categoria_producto')
 DROP TABLE INFORMADOS.categoria_producto
 
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'proveedor')
-DROP TABLE INFORMADOS.proveedor
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'cupon_por_venta')
+DROP TABLE INFORMADOS.cupon_por_venta
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'cupon')
+DROP TABLE INFORMADOS.cupon
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'descuento_por_venta')
+DROP TABLE INFORMADOS.descuento_por_venta
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'descuento_venta')
+DROP TABLE INFORMADOS.descuento_venta
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'venta')
+DROP TABLE INFORMADOS.venta
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'medio_pago_venta')
+DROP TABLE INFORMADOS.medio_pago_venta
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'canal_venta')
+DROP TABLE INFORMADOS.canal_venta
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'envio')
+DROP TABLE INFORMADOS.envio
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'metodo_envio')
+DROP TABLE INFORMADOS.metodo_envio
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'cliente')
+DROP TABLE INFORMADOS.cliente
 
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'zona')
 DROP TABLE INFORMADOS.zona
 
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'provincia')
 DROP TABLE INFORMADOS.provincia
-
----------------------------------------------------
--- ELIMINACION DE VISTAS EN CASO DE QUE EXISTAN
----------------------------------------------------
-
-
