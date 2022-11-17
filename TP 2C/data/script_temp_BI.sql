6e231f033a9fe11534363bc91dbebc92aa577a0e
Gonzalo <gorodriguez@frba.utn.edu.ar>
2022-11-16 21:39:18 -0300

    agrego tablas y sp

--- TP 2C/data/script_temp_BI.sql
+++ TP 2C/data/script_temp_BI.sql
@@ -37,7 +37,7 @@ END
 GO
 
 --Esta tabla va a tener las ventas realizadas, con la informacion de cada producto por separado, con sus cantidades y precio total de ese producto.
-CREATE TABLE INFORMADOS.BI_ventas_realizadas(
+CREATE TABLE INFORMADOS.BI_ventas_x_productos(
 id_venta bigint,
 id_cliente int,
 id_producto varchar(255),
@@ -46,16 +46,16 @@ cantidad int,
 precio_total_producto decimal(18,2)
 );
 
-IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_ventas_realizadas')
-	DROP PROCEDURE sp_migrar_bi_ventas_realizadas
+IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_ventas_x_productos')
+	DROP PROCEDURE sp_migrar_bi_ventas_x_productos
 
 GO
 
-CREATE PROCEDURE sp_migrar_bi_ventas_realizadas
+CREATE PROCEDURE sp_migrar_bi_ventas_x_productos
 AS
 BEGIN
 	PRINT 'Migracion de BI ventas realizadas'
-	INSERT INTO INFORMADOS.BI_ventas_realizadas(id_venta,id_cliente,id_producto,id_variante_producto,cantidad,precio_total_producto)
+	INSERT INTO INFORMADOS.BI_ventas_x_productos(id_venta,id_cliente,id_producto,id_variante_producto,cantidad,precio_total_producto)
 	SELECT ve.id_venta,ve.id_cliente,vp.id_producto,vp.id_variante_producto,pv.cantidad,(pv.cantidad*precio_unidad) as precio_total_producto
 	FROM INFORMADOS.venta ve
 	LEFT JOIN INFORMADOS.producto_por_venta pv
@@ -65,27 +65,119 @@ BEGIN
 
 END
 
+
+GO
+
+--Esta tabla va a tener exactamente la info de canal_venta del transaccional
+CREATE TABLE INFORMADOS.BI_canal_venta(
+id_canal_venta int,
+nombre_canal varchar(255),
+costo_canal decimal(18,2)
+);
+
+GO
+
+IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_canal_venta')
+	DROP PROCEDURE sp_migrar_bi_canal_venta
+
+GO
+
+CREATE PROCEDURE sp_migrar_bi_canal_venta
+AS
+BEGIN
+	PRINT 'Migracion de BI canal venta'
+	INSERT INTO INFORMADOS.BI_canal_venta(id_canal_venta,nombre_canal,costo_canal)
+	SELECT id_canal_venta,nombre,costo
+	FROM INFORMADOS.canal_venta
+END
+
+GO
+
+--Esta tabla va a tener las ventas relacionadas directamente con el canal y medio pago por el cual se vendio y el precio total de esa venta.
+CREATE TABLE INFORMADOS.BI_venta_total(
+id_venta int,
+id_canal_venta int,
+id_medio_pago_venta int,
+precio_total_venta decimal(18,2)
+);
+
+GO
+
+IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_venta_total')
+	DROP PROCEDURE sp_migrar_bi_venta_total
+
+GO
+
+CREATE PROCEDURE sp_migrar_bi_venta_total
+AS
+BEGIN
+	PRINT 'Migracion de BI venta total'
+	INSERT INTO INFORMADOS.BI_venta_total(id_venta,id_canal_venta,id_medio_pago_venta,precio_total_venta)
+	SELECT ve.id_venta,ve.id_canal,ve.id_medio_pago_venta,sum(vp.precio_total_producto)
+	FROM INFORMADOS.venta ve
+	LEFT JOIN INFORMADOS.BI_ventas_x_productos vp
+	ON ve.id_venta = vp.id_venta
+	GROUP BY ve.id_venta,ve.id_canal,ve.id_medio_pago_venta ORDER BY ve.id_venta asc
+END
+
+GO
+--Esta tabla va a tener la misma info que la tabla medio pago venta del transaccional
+CREATE TABLE INFORMADOS.BI_medio_pago_venta(
+id_medio_pago_venta int,
+nombre_medio_pago varchar(255),
+costo_medio_pago decimal(18,2)
+);
+GO
+
+
+--Esta tabla va a tener la misma info que la tabla medio pago compra del transaccional
+CREATE TABLE INFORMADOS.BI_medio_pago_compra(
+id_medio_pago_compra int,
+nombre_medio_pago varchar(255)
+);
+GO
+
+IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_medio_pago')
+	DROP PROCEDURE sp_migrar_bi_medio_pago
+
+GO
+
+CREATE PROCEDURE sp_migrar_bi_medio_pago
+AS
+BEGIN
+	
+	PRINT 'Migracion de BI medio de pago venta'
+	INSERT INTO INFORMADOS.BI_medio_pago_venta(id_medio_pago_venta,nombre_medio_pago,costo_medio_pago)
+	SELECT *
+	FROM INFORMADOS.medio_pago_venta
+
+	PRINT 'Migracion de BI medio de pago compra'
+	INSERT INTO INFORMADOS.BI_medio_pago_compra(id_medio_pago_compra,nombre_medio_pago)
+	SELECT *
+	FROM INFORMADOS.medio_pago_compra
+END
+
 GO
 
 --Esta tabla va a tener las compras realizadas, con la informacion de cada producto por separado, con sus cantidades y precio total de ese producto.
-CREATE TABLE INFORMADOS.BI_compras_realizadas(
+CREATE TABLE INFORMADOS.BI_compras_x_producto(
 id_compra int,
 id_producto varchar(255),
 id_variante_producto varchar(255),
 cantidad int, 
-precio_total_producto decimal(18,2)
+costo_total_producto decimal(18,2)
 );
 
-IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_compras_realizadas')
-	DROP PROCEDURE sp_migrar_bi_compras_realizadas
+IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_compras_x_producto')
+	DROP PROCEDURE sp_migrar_bi_compras_x_producto
 
 GO
 
-CREATE PROCEDURE sp_migrar_bi_compras_realizadas
+CREATE PROCEDURE sp_migrar_bi_compras_x_producto
 AS
 BEGIN
 	PRINT 'Migracion de BI compras realizadas'
-	INSERT INTO INFORMADOS.BI_compras_realizadas(id_compra,id_producto,id_variante_producto,cantidad,precio_total_producto)
+	INSERT INTO INFORMADOS.BI_compras_x_producto(id_compra,id_producto,id_variante_producto,cantidad,costo_total_producto)
 	SELECT cr.id_compra,vp.id_producto,vp.id_variante_producto,pc.cantidad,(pc.cantidad*pc.precio_unidad) as precio_total_producto
 	FROM INFORMADOS.compra cr
 	LEFT JOIN INFORMADOS.producto_por_compra pc
@@ -97,24 +189,57 @@ END
 
 GO
 
---Esta tabla va a tener las ventas relacionadas directamente con el canal por el cual se vendio y el precio total de esa venta.
-CREATE TABLE INFORMADOS.BI_canal_venta(
-id_venta bigint,
-canal varchar(255),
-precio_total_venta decimal(18,2)
+--Esta tabla va a tener las ventas relacionadas directamente con el canal y medio pago por el cual se vendio y el precio total de esa venta.
+CREATE TABLE INFORMADOS.BI_compra_total(
+id_compra int,
+id_medio_pago_compra int,
+costo_total_compra decimal(18,2)
 );
 
---Esta tabla va a tener aquellas ventas con su medio de pago y costo de transaccion
-CREATE TABLE INFORMADOS.BI_medio_pago_venta(
-id_venta int,
-medio_pago varchar(255),
-costo decimal(18,2)
-);
+GO
+
+IF EXISTS(SELECT [name] FROM sys.procedures WHERE [name] = 'sp_migrar_bi_compra_total')
+	DROP PROCEDURE sp_migrar_bi_compra_total
+
+GO
+
+CREATE PROCEDURE sp_migrar_bi_compra_total
+AS
+BEGIN
+	PRINT 'Migracion de BI compra total'
+	INSERT INTO INFORMADOS.BI_compra_total(id_compra,id_medio_pago_compra,costo_total_compra)
+	SELECT cr.id_compra,cr.id_medio_pago,sum(cp.costo_total_producto)
+	FROM INFORMADOS.compra cr
+	LEFT JOIN INFORMADOS.BI_compras_x_producto cp
+	ON cr.id_compra = cp.id_compra
+	GROUP BY cr.id_compra,cr.id_medio_pago ORDER BY cr.id_compra asc
+END
+
 
 /*
 Hasta acá estas tablas se usarian para realizar la primer Vista solicitada, y a su vez vienen a ser algunas dimensiones MINIMAS
 solicitadas en la consigna
 */
+
+SELECT * FROM INFORMADOS.BI_compra_total ct
+JOIN INFORMADOS.BI_tiempo_compra tc
+ON ct.id_compra=tc.id_compra
+
+SELECT * FROM INFORMADOS.BI_tiempo_venta
+
+
+
+CREATE VIEW vw_ganancia_mensual_canal
+AS
+SELECT tv.año,tv.mes,(SELECT )
+FROM INFORMADOS.BI_venta_total vt
+LEFT JOIN INFORMADOS.BI_canal_venta cv
+ON vt.id_canal_venta=cv.id_canal_venta
+LEFT JOIN INFORMADOS.BI_medio_pago_venta mp
+ON vt.id_medio_pago_venta=mp.id_medio_pago_venta
+JOIN INFORMADOS.BI_tiempo_venta tv
+ON vt.id_venta=tv.id_venta
+
 
 ------------------------------------------------------------------------
 
